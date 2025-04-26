f94e92cf24fc6f4ae681ce3a75d09a24f6132f04
Terryl Brown <tbrown414@live.com>
2025-04-26 09:30:46 -0700

    Auth customize token with JWT callback

--- auth.ts
+++ auth.ts
@@ -11,8 +11,8 @@ export const config = {
     error: "/sign-in",
   },
   session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60,
+    strategy: "jwt" as const,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -23,17 +23,22 @@ export const config = {
       },
       async authorize(credentials) {
         if (credentials == null) return null;
+
+        // Find user in database
         const user = await prisma.user.findFirst({
           where: {
             email: credentials.email as string,
           },
         });
 
+        // Check if user exists and if the password matches
         if (user && user.password) {
-          const isMatch = compareSync(
+          const isMatch = await compareSync(
             credentials.password as string,
             user.password
           );
+
+          // If password is correct, return user
           if (isMatch) {
             return {
               id: user.id,
@@ -43,14 +48,17 @@ export const config = {
             };
           }
         }
+        // If user does not exist or password does not match return null
         return null;
       },
     }),
   ],
   callbacks: {
     async session({ session, user, trigger, token }: any) {
-      //Set the user ID from the token
+      // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       // If there is an update, set the user name
       if (trigger === "update") {
@@ -58,6 +66,25 @@ export const config = {
       }
 
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+      }
+
+      // If user has no name then use the email
+      if (user?.name === "NO_NAME") {
+        token.name = user.email!.split("@")[0];
+
+        // Update database to reflect the token name
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { name: token.name },
+        });
+      }
+
+      return token;
     },
   },
 } satisfies NextAuthConfig;
